07115d91dc0bbee9330bbfe0eda1c58fa651d499
Barun Saha <barun.saha04@gmail.com>
2023-08-28 10:49:07 +0530

    Add hierarchical list of bullets from JSON to the slides

--- pptx_helper.py
+++ pptx_helper.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import json5
 import pptx
 import re
@@ -81,24 +81,16 @@ def generate_powerpoint_presentation(
         all_headers.append(title_shape.text)
         text_frame = body_shape.text_frame
 
-        for an_item in a_slide['bullet_points']:
-            item_type = type(an_item)
-            # print('Bullet point type:', item_type)
+        # The bullet_points may contain a nested hierarchy of JSON arrays
+        # In some scenarios, it may contain objects (dictionaries) because the LLM generated so
+        #  ^ The second scenario is not covered
 
-            if item_type is str:
-                paragraph = text_frame.add_paragraph()
-                paragraph.text = an_item
-                paragraph.level = 0
-            elif item_type is list:
-                for sub_item in an_item:
-                    if type(sub_item) is str:
-                        paragraph = text_frame.add_paragraph()
-                        paragraph.text = sub_item
-                        paragraph.level = 1
+        flat_items_list = get_flat_list_of_contents(a_slide['bullet_points'], level=0)
 
-        # background = slide.background
-        # background.fill.gradient()
-        # background.fill.gradient_angle = -225.0
+        for an_item in flat_items_list:
+            paragraph = text_frame.add_paragraph()
+            paragraph.text = an_item[0]
+            paragraph.level = an_item[1]
 
     # The thank-you slide
     last_slide_layout = presentation.slide_layouts[0]
@@ -111,9 +103,119 @@ def generate_powerpoint_presentation(
     return all_headers
 
 
+def get_flat_list_of_contents(items: list, level: int) -> List[Tuple]:
+    """
+    Flatten a (hierarchical) list of bullet points to a single list containing each item and its level.
+
+    :param items: A bullet point (string or list)
+    :param level: The current level of hierarchy
+    :return: A list of (bullet item text, hierarchical level) tuples
+    """
+
+    flat_list = []
+
+    for item in items:
+        if isinstance(item, str):
+            flat_list.append((item, level))
+        elif isinstance(item, list):
+            flat_list = flat_list + get_flat_list_of_contents(item, level + 1)
+
+    return flat_list
+
+
 if __name__ == '__main__':
+    # bullets = [
+    #     'Description',
+    #     'Types',
+    #     [
+    #         'Type A',
+    #         'Type B'
+    #     ],
+    #     'Grand parent',
+    #     [
+    #         'Parent',
+    #         [
+    #             'Grand child'
+    #         ]
+    #     ]
+    # ]
+
+    # output = get_flat_list_of_contents(bullets, level=0)
+    # for x in output:
+    #     print(x)
+
+    json_data = '''
+    {
+    "title": "Understanding AI",
+    "slides": [
+        {
+            "heading": "Introduction",
+            "bullet_points": [
+                "Brief overview of AI",
+                [
+                    "Importance of understanding AI"
+                ]
+            ]
+        },
+        {
+            "heading": "What is AI?",
+            "bullet_points": [
+                "Definition of AI",
+                [
+                    "Types of AI",
+                    [
+                        "Narrow or weak AI",
+                        "General or strong AI"
+                    ]
+                ],
+                "Differences between AI and machine learning"
+            ]
+        },
+        {
+            "heading": "How AI Works",
+            "bullet_points": [
+                "Overview of AI algorithms",
+                [
+                    "Types of AI algorithms",
+                    [
+                        "Rule-based systems",
+                        "Decision tree systems",
+                        "Neural networks"
+                    ]
+                ],
+                "How AI processes data"
+            ]
+        },
+        {
+            "heading": "Pros of AI",
+            "bullet_points": [
+                "Increased efficiency and productivity",
+                "Improved accuracy and precision",
+                "Enhanced decision-making capabilities",
+                "Personalized experiences"
+            ]
+        },
+        {
+            "heading": "Cons of AI",
+            "bullet_points": [
+                "Job displacement and loss of employment",
+                "Bias and discrimination",
+                "Privacy and security concerns",
+                "Dependence on technology"
+            ]
+        },
+        {
+            "heading": "Future Prospects of AI",
+            "bullet_points": [
+                "Advancements in fields such as healthcare and finance",
+                "Increased use"
+            ]
+        }
+    ]
+}'''
     generate_powerpoint_presentation(
-        json5.loads(open('examples/example_02_structured_output.json', 'r').read()),
+        json5.loads(json_data),
         as_yaml=False,
-        output_file_name='test.pptx'
+        output_file_name='test.pptx',
+        slides_template='Blank'
     )
