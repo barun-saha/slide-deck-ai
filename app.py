2d65634b0d86f5ba3fd1e8e0ca08858ad625d2fa
Barun Saha <barun.saha04@gmail.com>
2025-03-14 21:07:29 +0530

    Update HF token & permission message

--- app.py
+++ app.py
@@ -376,8 +376,9 @@ def set_up_chat_ui():
                     ' the input field on the sidebar to the left.'
                     '\n\nDon\'t have a token? Get your free'
                     ' [HF access token](https://huggingface.co/settings/tokens) now'
-                    ' and start creating your slide deck! Alternatively, choose a different LLM'
-                    ' and provider from the list.',
+                    ' and start creating your slide deck! For gated models, you may need to'
+                    ' visit the model\'s page and accept the terms or service.'
+                    '\n\nAlternatively, choose a different LLM and provider from the list.',
                     should_log=True
                 )
             else:
